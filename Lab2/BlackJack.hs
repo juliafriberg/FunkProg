d1abb5ea054ff11c6fb0a0ffca72ae1db6ee5328
Julia Friberg <juliafriberg93@gmail.com>
2016-11-08 16:47:22 +0100

    started implemented lab2b

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -80,33 +80,66 @@ winner guestHand bankHand
 -- These functions are a part of part B and are therefore not implemented yet
 
 (<+) :: Hand -> Hand -> Hand
-(<+) = undefined
+Empty <+ hand2 = hand2
+(Add card hand) <+ hand2 = (Add card (hand <+ hand2))
 
 prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
 prop_onTopOf_assoc p1 p2 p3 =
     p1<+(p2<+p3) == (p1<+p2)<+p3
 
 prop_size_onTopOf :: Hand -> Hand -> Bool
-prop_size_onTopOf = undefined
-
+prop_size_onTopOf hand1 hand2 = 
+    size (hand1 <+ hand2) == (size hand1 + size hand2)
 
 fullDeck :: Hand
-fullDeck = undefined
+fullDeck = (listToHand (cardsInSuit Hearts)) <+ (listToHand (cardsInSuit Diamonds)) <+ (listToHand (cardsInSuit Spades)) <+ (listToHand (cardsInSuit Clubs))
+
+cardsInSuit :: Suit -> [Card]
+cardsInSuit suit = [Card (Numeric a) suit | a <- [2..10]] ++ [Card val suit | val <- [Jack, Queen, King, Ace]]
+
+listToHand :: [Card] -> Hand
+listToHand [] = Empty
+listToHand [card] = (Add card Empty)
+listToHand (card:xs) = (Add card (listToHand xs))
+
+prop_size_FullDeck :: Bool
+prop_size_FullDeck = (size fullDeck) == 52
+
+prop_size_cardsInSuit :: Suit -> Bool
+prop_size_cardsInSuit suit = (length (cardsInSuit suit)) == 13
+
+prop_size_listToHand :: [Card] -> Bool
+prop_size_listToHand cards = (size (listToHand cards)) == length cards
 
 draw :: Hand -> Hand -> (Hand,Hand)
-draw = undefined
---error "draw: The deck is empty." when deck is empty
-
-first :: (a, b) -> a
-first (x,y) = x
+draw Empty _ = error "draw: The deck is empty."
+draw (Add firstCard deck) hand = (deck, (Add firstCard hand))
 
 playBank :: Hand -> Hand
-playBank = undefined
-{-playBank' deck bankHand 
-where (deck′,bankHand′) = draw deck bankHand-}
+playBank deck = playBank' deck Empty
+
+playBank' :: Hand -> Hand -> Hand
+playBank' deck bankHand 
+    | value bankHand' > 15 = bankHand'
+    | otherwise = playBank' deck' bankHand'
+    where (deck',bankHand') = draw deck bankHand
 
 shuffle' :: StdGen -> Hand -> Hand
-shuffle' = undefined
+shuffle' _ Empty = Empty
+shuffle' _ (Add card Empty) = (Add card Empty)
+shuffle' g hand  =  (Add card (shuffle' g' hand'))
+    where 
+        (index, g') = randomR (0, (size hand -1)) g
+        (hand', card) = removeCardAtIndex hand index
+
+removeCardAtIndex :: Hand -> Integer -> (Hand,Card)
+removeCardAtIndex Empty _ = error "removeCardAtIndex: The deck is empty"
+removeCardAtIndex (Add card hand) 0 = (hand,card)
+removeCardAtIndex (Add card hand) index 
+    | index > size hand || index < 0 = error "removeCardAtIndex: invalid index"
+    | otherwise = ((Add card hand'), card')
+    where (hand', card') = removeCardAtIndex hand (index-1)
+
 
 prop_shuffle_sameCards :: StdGen -> Card -> Hand -> Bool
 prop_shuffle_sameCards g c h =
@@ -114,11 +147,24 @@ prop_shuffle_sameCards g c h =
 
 
 belongsTo :: Card -> Hand -> Bool
-belongsTo = undefined
-{-
 c `belongsTo` Empty = False
-c `belongsTo` (Add c′ h) = c == c′ || c `belongsTo` h
--}
+c `belongsTo` (Add c' h) = ((c == c') || (c `belongsTo` h))
+
 
 prop_size_shuffle :: StdGen -> Hand -> Bool
 prop_size_shuffle = undefined
+
+
+{-implementation = Interface
+    { iEmpty    = empty
+      , iFullDeck = fullDeck
+      , iValue    = value
+      , iGameOver = gameOver
+      , iWinner   = winner 
+      , iDraw     = draw
+      , iPlayBank = playBank
+      , iShuffle  = shuffle
+    }
+
+main :: IO ()
+main = runGame implementation-}
