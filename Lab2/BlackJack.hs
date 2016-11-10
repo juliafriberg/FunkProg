c058fdd137c3861180e936d11767e118ebf31c78
Julia Friberg <juliafriberg93@gmail.com>
2016-11-10 15:48:08 +0100

    Cleaned up code from hLint hints

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -81,7 +81,7 @@ winner guestHand bankHand
 
 (<+) :: Hand -> Hand -> Hand
 Empty <+ hand2 = hand2
-(Add card hand) <+ hand2 = (Add card (hand <+ hand2))
+(Add card hand) <+ hand2 = Add card (hand <+ hand2)
 
 prop_onTopOf_assoc :: Hand -> Hand -> Hand -> Bool
 prop_onTopOf_assoc p1 p2 p3 =
@@ -92,28 +92,28 @@ prop_size_onTopOf hand1 hand2 =
     size (hand1 <+ hand2) == (size hand1 + size hand2)
 
 fullDeck :: Hand
-fullDeck = (listToHand (cardsInSuit Hearts)) <+ (listToHand (cardsInSuit Diamonds)) <+ (listToHand (cardsInSuit Spades)) <+ (listToHand (cardsInSuit Clubs))
+fullDeck = listToHand (cardsInSuit Hearts) <+ listToHand (cardsInSuit Diamonds) <+ listToHand (cardsInSuit Spades) <+ listToHand (cardsInSuit Clubs)
 
 cardsInSuit :: Suit -> [Card]
 cardsInSuit suit = [Card (Numeric a) suit | a <- [2..10]] ++ [Card val suit | val <- [Jack, Queen, King, Ace]]
 
 listToHand :: [Card] -> Hand
 listToHand [] = Empty
-listToHand [card] = (Add card Empty)
-listToHand (card:xs) = (Add card (listToHand xs))
+listToHand [card] = Add card Empty
+listToHand (card:xs) = Add card (listToHand xs)
 
 prop_size_FullDeck :: Bool
-prop_size_FullDeck = (size fullDeck) == 52
+prop_size_FullDeck = size fullDeck == 52
 
 prop_size_cardsInSuit :: Suit -> Bool
-prop_size_cardsInSuit suit = (length (cardsInSuit suit)) == 13
+prop_size_cardsInSuit suit = length (cardsInSuit suit) == 13
 
 prop_size_listToHand :: [Card] -> Bool
-prop_size_listToHand cards = (size (listToHand cards)) == length cards
+prop_size_listToHand cards = size (listToHand cards) == length cards
 
 draw :: Hand -> Hand -> (Hand,Hand)
 draw Empty _ = error "draw: The deck is empty."
-draw (Add firstCard deck) hand = (deck, (Add firstCard hand))
+draw (Add firstCard deck) hand = (deck, Add firstCard hand)
 
 playBank :: Hand -> Hand
 playBank deck = playBank' deck Empty
@@ -126,10 +126,10 @@ playBank' deck bankHand
 
 shuffle' :: StdGen -> Hand -> Hand
 shuffle' _ Empty = Empty
-shuffle' _ (Add card Empty) = (Add card Empty)
-shuffle' g hand  =  (Add card (shuffle' g' hand'))
+shuffle' _ (Add card Empty) = Add card Empty
+shuffle' g hand  =  Add card (shuffle' g' hand')
     where 
-        (index, g') = randomR (0, (size hand -1)) g
+        (index, g') = randomR (0, size hand -1) g
         (hand', card) = removeCardAtIndex hand index
 
 removeCardAtIndex :: Hand -> Integer -> (Hand,Card)
@@ -137,7 +137,7 @@ removeCardAtIndex Empty _ = error "removeCardAtIndex: The deck is empty"
 removeCardAtIndex (Add card hand) 0 = (hand,card)
 removeCardAtIndex (Add card hand) index 
     | index > size hand || index < 0 = error "removeCardAtIndex: invalid index"
-    | otherwise = ((Add card hand'), card')
+    | otherwise = (Add card hand', card')
     where (hand', card') = removeCardAtIndex hand (index-1)
 
 
@@ -148,7 +148,7 @@ prop_shuffle_sameCards g c h =
 
 belongsTo :: Card -> Hand -> Bool
 c `belongsTo` Empty = False
-c `belongsTo` (Add c' h) = ((c == c') || (c `belongsTo` h))
+c `belongsTo` (Add c' h) = (c == c') || (c `belongsTo` h)
 
 
 prop_size_shuffle :: StdGen -> Hand -> Bool
