418a324131bb9316a3953dc3c046ee7ad891dd51
Julia Friberg <julia@Julias-MacBook-Pro.local>
2016-12-13 10:57:54 +0100

    A graph is drawn if it is written correctly

--- Project/Calculator.hs
+++ Project/Calculator.hs
@@ -4,24 +4,27 @@ import Haste hiding (eval)
 import Haste.DOM
 import Haste.Events
 import Haste.Graphics.Canvas
+import Data.Maybe
 
 import Pages
 
 import Expr
 
-
-
 canWidth  = 300
 canHeight = 300
 
 readAndDraw :: Elem -> Canvas -> IO ()
-readAndDraw = undefined
+readAndDraw input canvas =
+    do
+        text <- getValue input
+        let expr = (fromJust (readExpr (fromJust text)))
+        render canvas (stroke (path (points expr 0.04 (canWidth, canHeight))))
 
 main = do
     -- Elements
     canvas  <- mkCanvas canWidth canHeight   -- The drawing area
     fx      <- mkHTML "<i>f</i>(<i>x</i>)="  -- The text "f(x)="
-    input   <- mkInput 20 "x"                -- The formula input
+    input   <- mkInput 40 "x"                -- The formula input
     draw    <- mkButton "Draw graph"         -- The draw button
       -- The markup "<i>...</i>" means that the text inside should be rendered
       -- in italics.
@@ -43,3 +46,15 @@ main = do
     onEvent draw  Click $ \_    -> readAndDraw input can
     onEvent input KeyUp $ \code -> when (code==13) $ readAndDraw input can
       -- "Enter" key has code 13
+
+points :: Expr -> Double -> (Int,Int) -> [Point]
+points exp scale (width,height) = calculatePoints 0
+
+    where 
+        calculatePoints x 
+            | x <= fromIntegral width = if y <= fromIntegral height && y >= 0 then (x, y):calculatePoints (x+1) else calculatePoints (x+1)
+            | otherwise = []
+            where y = realToPix (eval exp (pixToReal x))
+        pixToReal x = x * scale - (fromIntegral width) * scale / 2
+        realToPix y = (- y / scale) + (fromIntegral height) / 2
+
