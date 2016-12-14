f3346ca2a3168cea96d661bbd97ec719dc7a04db
Julia Friberg <julia@Julias-MacBook-Pro.local>
2016-12-14 10:31:40 +0100

    Comments and refactoring

--- Project/Expr.hs
+++ Project/Expr.hs
@@ -9,59 +9,78 @@ import System.Random
 import Control.Monad (replicateM)
 import Test.QuickCheck
 
-
+-- The recursive datatype for Expr, which consists of 
+-- floating point numbers, operands, variables and functions.
 data Expr = Lit Double
-    | Add Expr Expr
-    | Mul Expr Expr
+    | Op Operator Expr Expr
     | Var 
-    | Sin Expr
-    | Cos Expr
+    | F Function Expr
     deriving (Eq)
 
 instance Show Expr where
   show = showExpr
 
+-- The datatype for functions. Either Sin or Cos.
+data Function = Sin | Cos
+    deriving (Eq, Show)
+
+-- The datatype for operands. Either Add or Mul.
+data Operator = Mul | Add
+    deriving (Eq, Show)
+
+-- Takes a Function and returns the corresponding math function.
+getFun :: Floating a => Function -> (a -> a)
+getFun Sin = sin
+getFun Cos = cos
+
+-- Converts an expression to a string.
 showExpr :: Expr -> String
 showExpr (Lit n) = show n
-showExpr (Add a b) = showExpr a ++ " + " ++ showExpr b
-showExpr (Mul a b) = showFactor a ++ " * " ++ showFactor b
+showExpr (Op Add a b) = showExpr a ++ " + " ++ showExpr b
+showExpr (Op Mul a b) = showFactor a ++ " * " ++ showFactor b
+    where 
+        -- To make sure parantheses are where they should be.
+        showFactor :: Expr -> String
+        showFactor (Op Add a b) = "(" ++ showExpr (Op Add a b) ++ ")" 
+        showFactor e = showExpr e
 showExpr Var = "x"
-showExpr (Sin a) = "sin " ++ showBrackets a 
-showExpr (Cos a) = "cos " ++ showBrackets a 
+showExpr (F f a) = (map toLower (show f)) ++ " " ++ showBrackets a
+    where
+        -- To make sure parantheses are where they should be.
+        showBrackets :: Expr -> String
+        showBrackets (Op ops a b) = "(" ++ showExpr (Op ops a b) ++ ")" 
+        showBrackets e = showExpr e 
 
-showFactor :: Expr -> String
-showFactor (Add a b) = "(" ++ showExpr (Add a b) ++ ")" 
-showFactor e = showExpr e
-
-showBrackets :: Expr -> String
-showBrackets (Add a b) = "(" ++ showExpr (Add a b) ++ ")" 
-showBrackets (Mul a b) = "(" ++ showExpr (Mul a b) ++ ")" 
-showBrackets e = showExpr e 
-
-
+-- Calculates the value of an expression.
 eval :: Expr -> Double -> Double
 eval (Lit n) _ = n
-eval (Add a b) x = eval a x + eval b x
-eval (Mul a b) x = eval a x * eval b x
+eval (Op Add a b) x = eval a x + eval b x
+eval (Op Mul a b) x = eval a x * eval b x
 eval Var x = x
-eval (Sin a) x = sin (eval a x)
-eval (Cos a) x = cos (eval a x)  
+eval (F f a) x = (getFun f) (eval a x)
 
+-- Interprets the string as an expression. 
+-- Returns Just expression if succeeds, otherwise Nothing. 
 readExpr :: String -> Maybe Expr              
 readExpr s = let s' = filter (not.isSpace) s
              in case parse expr s' of
                      Just (e,"") -> Just e
                      _           -> Nothing
 
+-- Parse a list of items with separators
+-- Taken from the lecture in week 4 about parsing.
 leftAssoc :: (t->t->t) -> Parser t -> Parser sep -> Parser t
 leftAssoc op item sep = do is <- chain item sep
                            return (foldl1 op is)
 
+-- Parsers for expr, term and factor.
+-- expr and term are taken from the lecture in week 4 about parsing. 
+-- factor has been changed by adding parsers for variables and functions.
 expr, term, factor :: Parser Expr
 
-expr = leftAssoc Add term (char '+')
+expr = leftAssoc (Op Add) term (char '+')
 
-term = leftAssoc Mul factor (char '*')
+term = leftAssoc (Op Mul) factor (char '*')
 
 factor = (Lit <$> num) 
         <|> var 
@@ -84,63 +103,68 @@ string (c:s)    = do
                     c' <- char c
                     s' <- string s
                     return (c':s')  
-
+-- Parser for sin
 sinParse = do   s <- string "sin"
-                return Sin  
+                return (F Sin)  
 
+-- Parser for cos
 cosParse = do   s <- string "cos"
-                return Cos  
+                return (F Cos)  
 
-prop_ShowReadExpr :: Expr -> Bool
-prop_ShowReadExpr ex = showExpr (fromJust (readExpr $ showExpr ex)) == showExpr ex
-
+-- Simplifies an expression. 
+-- Simplifies x + 0, 0 * x, 1 * x and similar terms.
+-- Simplifies subexpresssions not involving variables to 
+-- their smallest representation. 
+-- Simplifies 2 * x + 4 * x to 6 * x and similar expressions.
+-- Simplifies (2 * x) * (4 * x) to 8 * x * x an similar expressions.
 simplify :: Expr -> Expr
-simplify (Add a b) = simplifiedAdd (simplify a) (simplify b)
-simplify (Mul a b) = simplifiedMul (simplify a) (simplify b)
-simplify (Sin a) = simplifiedSin (simplify a)
-simplify (Cos a) = simplifiedCos (simplify a) 
+simplify (Op Add a b) = simplifiedAdd (simplify a) (simplify b)
+    where
+        -- Simplifies addition expressions
+        simplifiedAdd :: Expr -> Expr -> Expr
+        simplifiedAdd (Lit 0) e = e
+        simplifiedAdd e (Lit 0) = e
+        simplifiedAdd (Lit n) (Lit m) = Lit (n+m)
+        simplifiedAdd Var Var = Op Mul (Lit 2) Var
+        simplifiedAdd (Op Mul (Lit n) e1) e2 
+                | e1 == e2 = Op Mul (Lit (n+1)) e1
+        simplifiedAdd (Op Mul (Lit n) e1) (Op Mul (Lit m) e2) 
+                | e1 == e2 = Op Mul (Lit (n+m)) e1
+        simplifiedAdd e1 e2 = Op Add e1 e2
+
+simplify (Op Mul a b) = simplifiedMul (simplify a) (simplify b)
+    where 
+        -- Simplifies multiplication expressions
+        simplifiedMul :: Expr -> Expr -> Expr
+        simplifiedMul (Lit 0) _ =  Lit 0
+        simplifiedMul _ (Lit 0) = Lit 0
+        simplifiedMul (Lit 1) e = e
+        simplifiedMul e (Lit 1) = e
+        simplifiedMul (Lit n) (Lit m) = Lit (n*m)
+        simplifiedMul (Op Mul (Lit n) e1) e2 
+                | e1 == e2 = Op Mul (Lit n) (Op Mul e1 e2)
+        simplifiedMul (Op Mul (Lit n) e1) (Op Mul (Lit m) e2) 
+                | e1 == e2 = Op Mul (Lit (n*m)) (Op Mul e1 e2)
+        simplifiedMul e1 (Lit n) = Op Mul (Lit n) e1
+        simplifiedMul e1 e2 = Op Mul e1 e2
+
+simplify (F f a) = simplifiedFun (F f (simplify a))
+    where
+        -- Simplifies function expressions
+        simplifiedFun :: Expr -> Expr
+        simplifiedFun (F f (Lit n)) = Lit ((getFun f) n)
+        simplifiedFun (F f a) = (F f a)
+
 simplify e = e 
 
-simplifiedAdd :: Expr -> Expr -> Expr
-simplifiedAdd (Lit 0) e = e
-simplifiedAdd e (Lit 0) = e
-simplifiedAdd (Lit n) (Lit m) = Lit (n+m)
-simplifiedAdd Var Var = Mul (Lit 2) Var
-simplifiedAdd (Mul (Lit n) e1) e2 | e1 == e2 = Mul (Lit (n+1)) e1
-simplifiedAdd (Mul (Lit n) e1) (Mul (Lit m) e2) | e1 == e2 = Mul (Lit (n+m)) e1
-simplifiedAdd e1 e2 = Add e1 e2
-
-simplifiedMul :: Expr -> Expr -> Expr
-simplifiedMul (Lit 0) _ =  Lit 0
-simplifiedMul _ (Lit 0) = Lit 0
-simplifiedMul (Lit 1) e = e
-simplifiedMul e (Lit 1) = e
-simplifiedMul (Lit n) (Lit m) = Lit (n*m)
-simplifiedMul (Mul (Lit n) e1) e2 | e1 == e2 = Mul (Lit n) (Mul e1 e2)
-simplifiedMul (Mul (Lit n) e1) (Mul (Lit m) e2) | e1 == e2 = Mul (Lit (n*m)) (Mul e1 e2)
-simplifiedMul e1 (Lit n) = Mul (Lit n) e1
-simplifiedMul e1 e2 = Mul e1 e2
-
-simplifiedSin :: Expr -> Expr
-simplifiedSin (Lit n) = Lit (sin n)
-simplifiedSin e = Sin e
-
-simplifiedCos :: Expr -> Expr
-simplifiedCos (Lit n) = Lit (cos n)
-simplifiedCos e = Cos e
-
-prop_simplify :: Expr -> Double -> Bool
-prop_simplify e d = eval e d == eval (simplify e) d
-
--- TODO add property for checking if it simplifies enough.
 
 differentiate :: Expr -> Expr
 differentiate e = simplify $ differentiate' (simplify e)
     where
         differentiate' (Lit _) = Lit 0
         differentiate' Var = Lit 1
-        differentiate' (Sin e) = Mul (differentiate' e) (Cos e)
-        differentiate' (Cos e) = Mul (Mul (Lit (-1)) (differentiate' e)) (Sin e)
-        differentiate' (Add a b) = Add (differentiate' a) (differentiate' b)
-        differentiate' (Mul a b) = Add (Mul (differentiate' a) b) (Mul a (differentiate' b))
+        differentiate' (F Sin e) = Op Mul (differentiate' e) (F Cos e)
+        differentiate' (F Cos e) = Op Mul (Op Mul (Lit (-1)) (differentiate' e)) (F Sin e)
+        differentiate' (Op Add a b) = Op Add (differentiate' a) (differentiate' b)
+        differentiate' (Op Mul a b) = Op Add (Op Mul (differentiate' a) b) (Op Mul a (differentiate' b))
 
--- Project/ExprQC.hs
+++ Project/ExprQC.hs
@@ -3,30 +3,40 @@ module ExprQC where
 import Test.QuickCheck
 import Expr
 import Data.List
+import Data.Maybe
 
+-- A generator for expressions. 
 arbExpr :: Int -> Gen Expr
 arbExpr n 
-    | n == 0 = rNum
-    | otherwise = oneof [rAdd, rMul, return Var, rSin, rCos, rNum]
+    | n == 0 = oneof [return Var, rNum]
+    | otherwise = oneof [rOp, return Var, rFun, rNum]
     where
         rNum = do
-            a <- elements [1..10 :: Double] -- TODO fix 
-            return $ Lit a 
-        rAdd = do
+            a <- elements [1..10 :: Double]
+            b <- elements [1..10 :: Double] 
+            return $ Lit (a + b / 10.0)  
+        rOp = do
+            operand <- oneof [return Add, return Mul]
             term1 <- arbExpr size
             term2 <- arbExpr size
-            return (Add term1 term2)
-        rMul = do
-            factor1 <- arbExpr size
-            factor2 <- arbExpr size
-            return (Mul factor1 factor2) 
-        rSin = do
+            return (Op operand term1 term2)
+        rFun = do
+            fun <- oneof [return Sin, return Cos]
             expr <- arbExpr (n-1)
-            return (Sin expr)
-        rCos = do
-            expr <- arbExpr (n-1)
-            return (Cos expr)
+            return (F fun expr)
         size = n `div` 2
 
 instance Arbitrary Expr where
   arbitrary = sized arbExpr
+
+-- Tests that readExpr and showExpr works.
+prop_ShowReadExpr :: Expr -> Bool
+prop_ShowReadExpr ex = showExpr (fromJust (readExpr $ showExpr ex)) == showExpr ex
+
+-- Tests that the simplified version has the same value 
+-- as the original expression.
+prop_simplify :: Expr -> Double -> Bool
+prop_simplify e d = eval e d == eval (simplify e) d
+
+
+-- TODO add property for checking if it simplifies enough.
